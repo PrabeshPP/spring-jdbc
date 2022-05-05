2d59e1ce51a274784ed38b4dd256a61fb46571ce
Prabesh <prabesh.bista369@gmail.com>
2022-05-05 17:48:37 +0545

    Connecting to MYSQL Database

---
+++ src/main/java/org/springjdbc/Student.java
@@ -0,0 +1,16 @@
+package org.springjdbc;
+
+public class Student {
+    private int id;
+    private String name;
+    private String city;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+}
