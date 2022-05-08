265ec997d16bd47a62cbcdd6896090d716a93df2
Prabesh <prabesh.bista369@gmail.com>
2022-05-08 09:38:50 +0545

    Practicing he SpringJDBC

--- src/main/java/entity/Person.java
+++ src/main/java/entity/Person.java
@@ -5,4 +5,31 @@ public class Person {
     private String Name;
     private int Age;
 
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public Person(){
+
+    }
 }
--- src/main/java/org/springjdbc/App.java
+++ src/main/java/org/springjdbc/App.java
@@ -1,12 +1,21 @@
 package org.springjdbc;
 
 
-public class App 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class App
 {
     public static void main( String[] args )
     {
 
-        System.out.println("Hi");
+        ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
+        JdbcTemplate jdbcTemplate=context.getBean("jdbctemplate",JdbcTemplate.class);
+        String query="insert into Person(Id,Name,Age) values(?,?,?)";
+        int result=jdbcTemplate.update(query,101,"Pirate",400);
+        System.out.println(result);
+
 
 
     }
